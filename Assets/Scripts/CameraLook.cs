52a9c048fe000b51434b9b4e9e9c78d08bd38aad
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-05-01 12:04:35 +1000

    end of class

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
+    public bool isCursorHidden = true;
+    public float minPitch = 20f, MaxPitch = 80f;
+    public Vector2 speed = new Vector2(120f, 120f);
+
+    private Vector3 euler;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Is the cursor supposed to be hidden
+        if (isCursorHidden)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        euler = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //rotate the camera based on mouse movement
+        euler.y += Input.GetAxis("Mouse X") * speed.x * Time.deltaTime;
+        euler.x -= Input.GetAxis("Mouse Y") * speed.y * Time.deltaTime;
+
+        //Clamp the value
+        euler.x = Mathf.Clamp(euler.x, minPitch, MaxPitch);
+        //Rotate the camera and player
+        transform.parent.localEulerAngles = new Vector3(0, euler.y, 0);
+        transform.localEulerAngles = new Vector3(euler.x, 0, 0);
     }
 }
